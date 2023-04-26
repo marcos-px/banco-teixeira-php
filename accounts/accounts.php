81b7d1b63f6e1b35dc30d7c5216553b17bce3f11
Marcos <marcxstx@gmail.com>
2023-04-26 17:19:46 -0300

    Avançando a lógica

--- accounts/accounts.php
+++ accounts/accounts.php
@@ -1,20 +1,25 @@
 <?php
 
-$conta1 = [
-    'titular' => 'Marcos',
-    'saldo' => 1000
-];
-$conta2 = [
-    'titular' => 'Léo',
-    'saldo' => 10000
-];
-$conta3 = [
-    'titular' => 'Clara',
-    'saldo' => 30000
+$contasCorrentes = [
+    "123.456.789-10" => [
+        'titular' => 'Marcos',
+        'saldo' => 1000
+    ],
+    "123.456.789-11" => [
+        'titular' => 'Léo',
+        'saldo' => 10000
+    ], 
+    "123.456.789-12" => [
+        'titular' => 'Clara',
+        'saldo' => 30000
+    ],
 ];
 
-$contasCorrentes = [$conta1, $conta2, $conta3];
+$contasCorrentes["123.151.456-15"] = [
+    'titular' => 'Claudia',
+    'saldo' => 2000
+];
 
-for ($i = 0; $i < count($contasCorrentes); $i++) {
-    echo $contasCorrentes[$i]['titular'] . PHP_EOL;
-};
+foreach ($contasCorrentes as $cpf => $conta) {
+    echo $conta['titular'] . ' - ' . $cpf . PHP_EOL;
+}
