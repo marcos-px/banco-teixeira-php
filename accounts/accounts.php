39c015d5fbe9171ef2973fb946821759e58dbe78
Marcos <marcxstx@gmail.com>
2023-04-26 19:34:38 -0300

    Adicionando a função de depositar.

--- accounts/accounts.php
+++ accounts/accounts.php
@@ -1,20 +1,32 @@
 <?php
 
-function sacar($conta, $valorASacar)
+function sacar(array $conta, float $valorASacar): array
 {
-    
+
     if ($valorASacar > $conta['saldo']) {
-        exibeMensagem ("Saldo Insuficiente");
+        exibeMensagem("Saldo Insuficiente");
     } else {
         $conta['saldo'] -= $valorASacar;
     }
     return $conta;
 }
 
-function exibeMensagem ($mensagem) 
+function depositar(array $conta, $valorADepositar): array
+{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depositos precisam ser positivos");
+    }
+    return $conta;
+}
+
+function exibeMensagem(string $mensagem)
 {
     echo $mensagem . PHP_EOL;
-};
+}
+;
+
 
 $contasCorrentes = [
     "123.456.789-10" => [
@@ -24,7 +36,7 @@ $contasCorrentes = [
     "123.456.789-11" => [
         'titular' => 'Léo',
         'saldo' => 300
-    ], 
+    ],
     "123.456.789-12" => [
         'titular' => 'Clara',
         'saldo' => 30000
@@ -33,8 +45,9 @@ $contasCorrentes = [
 
 
 $contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], 500);
-$contasCorrentes['123.456.789-11'] = sacar($contasCorrentes['123.456.789-11'], 500);
+
+$contasCorrentes['123.456.789-12'] = depositar($contasCorrentes['123.456.789-12'], 500);
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    exibeMensagem( $cpf . ' ' . $conta['titular'] . ' ' . $conta['saldo']);
-};
+    exibeMensagem($cpf . ' ' . $conta['titular'] . ' ' . $conta['saldo']);
+}
